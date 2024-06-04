98d554b8c717885dd6a8bacc1129cb6bcfa241b8
999Ralex999 <140698368+999Ralex999@users.noreply.github.com>
2024-06-04 15:48:19 +0300

    2_part_feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
+import { Section } from './section/section';
+import { FeedbackOptions } from './feedback/feedback';
+import { Statistics } from './statistics/statistics';
+import { Notification } from './notification/notification';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
-        display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        display: 'flex',
+        color: '#010101',
+        backgroundColor: '#f0f0f0',
       }}
     >
-      React homework template
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={this.state}
+          onLeaveFeedback={this.onLeaveFeedback}
+        />
+        {this.total() ? (
+          <Statistics
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={this.total()}
+            positivePercentage={this.positivePercentage()}
+          />
+        ) : (
+          <Notification message="No feedback is available" />
+        )}
+      </Section>
     </div>
   );
 };
